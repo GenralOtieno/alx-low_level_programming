fd3a0107cab34ab741ad82b189d8a9c8237c88f6
GenralOtieno <adikaotiphilip001@gmail.com>
2023-09-12 01:50:11 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,4 @@ printf("Size of a long int; %lu bytes(s)\n",(unsigned long)sizeof(c));
 printf("Size of a long long int; %lu bytes(s)\n",(unsigned long)sizeof(d));
 printf("size of float; %lu bytes(s)\n",(unsigned long)sizeof(f));
 return (0);
-
+}
